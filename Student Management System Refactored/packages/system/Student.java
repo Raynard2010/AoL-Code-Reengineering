fbbf1ccc490db8bb2808c21f9afc18558aeea0ed
Raynard Chandrawangsa <145453934+Raynard2010@users.noreply.github.com>
2025-05-10 01:48:21 +0700

    Add files via upload

---
+++ Student Management System Refactored/packages/system/Student.java
@@ -0,0 +1,43 @@
+package packages.system;
+
+    // ---------------------------------------------- STUDENT CLASS ----------------------------------------------
+
+public class Student {
+    private String name;
+    private int ID;
+    private double GPA;
+    private String year;
+    private String department;
+
+    public Student() {}
+
+    public Student(int ID, String name, double GPA, String year, String department) {
+        this.ID = ID;
+        this.name = name;
+        this.GPA = GPA;
+        this.year = year;
+        this.department = department;
+    }
+
+	public String getName() {
+		return name;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public double getGPA() {
+		return GPA;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+    
+    
+}
