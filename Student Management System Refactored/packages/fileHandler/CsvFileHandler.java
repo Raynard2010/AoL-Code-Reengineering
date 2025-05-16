89c1d7d6356785c60ead64cecf2c1528ad07b400
Raynard Chandrawangsa <145453934+Raynard2010@users.noreply.github.com>
2025-05-16 16:04:20 +0700

    Update CsvFileHandler.java

--- Student Management System Refactored/packages/fileHandler/CsvFileHandler.java
+++ Student Management System Refactored/packages/fileHandler/CsvFileHandler.java
@@ -9,21 +9,19 @@ import packages.system.*;
 public class CsvFileHandler {
     private static final String OUTPUT_CSV_FILE = "Final_Students.csv";
 
-    // Convert a CSV line to a Student object
     public static Student fromCSV(String csvLine) {
         csvLine = csvLine.replaceAll("\"", "");
         String[] parts = csvLine.split(",");
 
-        return new Student(
-                Integer.parseInt(parts[0]),     // ID
-                parts[1],                       // Name
-                Double.parseDouble(parts[2]),   // GPA
-                parts[3],                       // Year
-                parts[4]                        // Department
-        );
+        Student student = new Student();
+        student.setID(Integer.parseInt(parts[0]));
+        student.setName(parts[1]);
+        student.setGPA(Double.parseDouble(parts[2]));
+        student.setYear(parts[3]);
+        student.setDepartment(parts[4]);
+        return student;
     }
 
-    // Read the student system from a csv file
     public static StudentSystem readCsvFile() {
         StudentSystem students = new StudentSystem();
         Scanner scanner = new Scanner(System.in);
@@ -42,15 +40,15 @@ public class CsvFileHandler {
                 Student student = fromCSV(line);
                 boolean isUnique = true;
                 for (Student s : students.getStudentList()) {
-                    if (s.ID == student.ID || s.name.equals(student.name)) {
+                    if (s.getID() == student.getID() || s.getName().equals(student.getName())) {
                         isUnique = false;
                         break;
                     }
                 }
 
                 if (isUnique) {
-                    if (student.year.equals("First") || student.year.equals("Second")) student.department = "General";
-                    students.addStudent(student.name, student.ID, student.GPA, student.year, student.department, true);
+                    if (student.getYear().equals("First") || student.getYear().equals("Second")) student.setDepartment("General");
+                    students.addStudent(student, true);
                 }
             }
 
@@ -62,8 +60,11 @@ public class CsvFileHandler {
         }
         return students;
     }
-
-    // Write the student system to a csv file
+    
+    public static <T> void writeDataToCsvFile(FileWriter writer, T data) throws IOException{
+    	writer.write("\"" + data + "\",");
+    }
+    
     public static void writeCsvFile(StudentSystem students) {
         try (FileWriter writer = new FileWriter(OUTPUT_CSV_FILE)) {
             if (students.getStudentList().isEmpty()) {
@@ -75,11 +76,12 @@ public class CsvFileHandler {
 
             writer.write("\"ID\",\"Name\",\"GPA\",\"Year\",\"Department\"\n");
             for (Student student : students.getStudentList()) {
-                writer.write("\"" + student.ID + "\",");
-                writer.write("\"" + student.name + "\",");
-                writer.write("\"" + student.GPA + "\",");
-                writer.write("\"" + student.year + "\",");
-                writer.write("\"" + student.department + "\"\n");
+            	writeDataToCsvFile(writer, student.getID());
+            	writeDataToCsvFile(writer, student.getName());
+            	writeDataToCsvFile(writer, student.getGPA());
+            	writeDataToCsvFile(writer, student.getYear());
+            	writeDataToCsvFile(writer, student.getDepartment());
+            	writer.write("\n");
             }
             writer.flush();
             System.out.println("\nCSV file written successfully.");
